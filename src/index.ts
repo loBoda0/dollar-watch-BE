0011f53815a837d3043b0ce93717cb2e2e7be5cd
loBoda0 <martin.loboda02@gmail.com>
2025-05-26 18:48:03 +0200

    prettier and eslint setup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { Elysia, t } from "elysia";
-import { getData, postData } from "./routes/items";
+import { Elysia, t } from 'elysia';
+import { getData, postData } from './routes/items';
 
 const app = new Elysia()
-    .get("/", () => getData)
-    .post("/", ({ body }) => postData(body), {
+    .get('/', () => getData)
+    .post('/', ({ body }) => postData(body), {
         body: t.Object({
             firstName: t.String(),
-            lastName: t.String()
-        })
+            lastName: t.String(),
+        }),
     })
     .listen(8080);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
